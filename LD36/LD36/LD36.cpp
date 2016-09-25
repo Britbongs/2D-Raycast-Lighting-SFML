87f1a1ac3dae1c0523cfb51ad1e05350156dc961
A. M. Walker <b4014242@my.shu.ac.uk>
2016-09-25 14:14:48 +0100

    Work on debug printf

--- LD36/LD36/LD36.cpp
+++ LD36/LD36/LD36.cpp
@@ -11,7 +11,7 @@ int main(void)
 {
 	Game GameInstance;
 
-	DebugPrint("fuck you");
+	//DebugPrint("fuck you");
 
 	if (!GameInstance.InitGame())
 	{
--- LD36/LD36/include/DebugHelper.h
+++ LD36/LD36/include/DebugHelper.h
@@ -3,9 +3,26 @@
 #include "stdafx.h"
 #include <fstream>
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN 1
+#endif 
+#include <Windows.h>
+
+
 #ifndef PLAYABLE_BUILD
 static void PrintToDebug(const sf::String& LogName, const sf::String& DebugMessage)
 {
 	std::wcout << LogName.toWideString() << L" : " << DebugMessage.toWideString() << std::endl;
 }
+
+int VDebugPrintF(const char* Format, va_list ArgList)
+{
+	const Uint32 MAX_CHARS = 1024;
+	static char s_buffer[MAX_CHARS];
+
+	int charsWritten = vsnprintf(s_buffer, MAX_CHARS, Format, ArgList);
+
+
+}
+
 #endif
